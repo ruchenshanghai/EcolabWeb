f8974a343b4d859b20dd7d0e2e81ed357c5437e3
濡尘赏海 <996956441@qq.com>
2017-06-21 18:40:29 +0800

    front 80%

--- public/js/detail.js
+++ public/js/detail.js
@@ -9,6 +9,11 @@ $(document).ready(function () {
     let mainDataID = paramArray[length - 1];
     console.log(mainDataID);
 
+    let username;
+    $.post('/username').then(name => {
+       username = name;
+       $('#username').html(username);
+    });
     $.post('/data/' + mainDataID).then(mainData => {
         console.log(mainData);
     });
